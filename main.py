f40fa76aff621437f74d8db9a6a6d8c3bb05545a
jcarlos8511 <104638968+jcarlos8511@users.noreply.github.com>
2023-10-14 13:20:35 -0400

    Prueba_desde_Casa
    Prueba_Inicial

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+@app.post("/")
+
+def process_order():
+    return{'order':[
+        {"id":1, "item":"Laptop", "quantity":1, "price":999.99, "status":"completed"},
+        {"id":2, "item":"Smartphone", "quantity":2,"price":499.95, "status":"pending"},
+        {"id":3, "item":"Headphones", "quantity":3,"price":99.90, "status":"completed"},
+        {"id":4, "item":"Mause", "quantity":4,"price":24.99, "status":"canceled"}
+        ], 
+        "criterion":"completed"
+    }
+
+    
